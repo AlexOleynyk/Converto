0484dfe5cb3678b627c1806d81af1614fa9f45f9
a.oleynik <a.oleynik@andersenlab.com>
2021-07-29 10:42:00 +0300

    Simplify logic for getting whole part for decimal

--- Converto/Extensions/Decimal+Extension.swift
+++ Converto/Extensions/Decimal+Extension.swift
@@ -1,12 +1,5 @@
 import Foundation
 
 extension Decimal {
-    var whole: Decimal { self < 0 ? rounded(roundingMode: .up) : rounded(roundingMode: .down) }
-
-    func rounded(scale: Int = 2, roundingMode: NSDecimalNumber.RoundingMode = .plain) -> Decimal {
-        var amountBefore = self
-        var result: Decimal = self
-        NSDecimalRound(&result, &amountBefore, scale, roundingMode)
-        return result
-    }
+    var whole: Decimal { Decimal(NSDecimalNumber(decimal: self).intValue) }
 }
