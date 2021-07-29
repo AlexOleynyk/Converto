d83d4fa1c4059b84e6222b3eaf9b1ede01904b7a
a.oleynik <a.oleynik@andersenlab.com>
2021-07-29 10:23:18 +0300

    Add random network delay

--- Converto/NetworkDelaySimulation.swift
+++ Converto/NetworkDelaySimulation.swift
@@ -1,6 +1,9 @@
 import Foundation
 
-func simulateNetworkDelay(delayInSeconds: TimeInterval = 1, completion: @escaping () -> Void) {
+func simulateNetworkDelay(
+    delayInSeconds: TimeInterval = .random(in: 0.5...1.5),
+    completion: @escaping () -> Void
+) {
     DispatchQueue.main.asyncAfter(deadline: .now() + delayInSeconds) {
         completion()
     }
